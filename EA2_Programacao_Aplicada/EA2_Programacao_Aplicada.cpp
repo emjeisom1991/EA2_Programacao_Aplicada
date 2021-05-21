c1fc4e8cefc7f7b39d05f4f4a3d4c1023a5b8bc1
Emanuel Jeisom <emjeisom.desenvolvimento@gmail.com>
2021-05-21 20:04:41 -0300

    Implementado função main e finalizado o codigo

--- EA2_Programacao_Aplicada/EA2_Programacao_Aplicada.cpp
+++ EA2_Programacao_Aplicada/EA2_Programacao_Aplicada.cpp
@@ -1,15 +1,35 @@
 // Resolução da Segunda Atividade Avaliativa da Disciplina de Programação Aplicada
-//Nome da atividade: EA2_Programacao_Aplicada
+// Nome da atividade: EA2_Programacao_Aplicada
+// Feito consulta no stackoverflow no link https://pt.stackoverflow.com/questions/34060/como-criar-um-vetor-de-tamanho-vari%C3%A1vel
+// para verificar como poderia usar um vetor com tamanho variavel.
+
 #include <iostream>
 
 using namespace std;
+
 void numsMaxMin(int* nums, int num, int* min, int* max);
 
 int main()
 {
+    int num, * nums, min, max;
 
-	cout << endl << endl;
-	return 0;
+    cin >> num;
+    nums = (int*)malloc(num * sizeof(int));
+
+    for (int i = 0; i < num; i++) {
+        cin >> nums[i];
+    }
+
+    for (int i = 0; i < num; i++) {
+        cout << nums[i] << ",";
+    }
+
+    cout << endl;
+    numsMaxMin(nums, num, &min, &max);
+    cout << "Max=" << max << ", Min=" << min << ".";
+
+    cout << endl << endl;
+    return 0;
 }
 
 void numsMaxMin(int* nums, int num, int* min, int* max) {
