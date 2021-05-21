83a59220f1d84714e6d43f21309fa4470989af95
Emanuel Jeisom <emjeisom.desenvolvimento@gmail.com>
2021-05-21 19:19:28 -0300

    Criada função sem retorno para verificar quais numeros são maiores e menores do vetor.

--- EA2_Programacao_Aplicada/EA2_Programacao_Aplicada.cpp
+++ EA2_Programacao_Aplicada/EA2_Programacao_Aplicada.cpp
@@ -3,12 +3,26 @@
 #include <iostream>
 
 using namespace std;
+void numsMaxMin(int* nums, int num, int* min, int* max);
 
 int main()
 {
 
-
-
 	cout << endl << endl;
 	return 0;
+}
+
+void numsMaxMin(int* nums, int num, int* min, int* max) {
+
+    *min = nums[0];
+    *max = nums[0];
+
+    for (int i = 1; i < num; i++) {
+        if (nums[i] > *max) {
+            *max = nums[i];
+        }
+        else if (nums[i] < *min) {
+            *min = nums[i];
+        }
+    }
 }
